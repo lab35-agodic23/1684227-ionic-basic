417bae231a25cba7631a444877278dd9fe59415f
DaoVega <diego.doger60@gmail.com>
2023-09-02 16:52:32 -0600

    Esta es la Practica 4

--- Practica 3/script.js
+++ Practica 3/script.js
@@ -1,52 +1,20 @@
-const inputUser = document.getElementById('user');
-const inputPassword = document.getElementById('password');
+const presupuesto = [];
 
-const buttonLogin = document.getElementById('login');
-const buttonRegister = document.getElementById('registrar');
+function guardar() {
+  const tipoDeGasto = document.querySelector('ion-input[label="Tipo de Gasto"]').value;
+  const monto = document.querySelector('ion-input[label="Monto"]').value;
 
-const messageStatus = document.getElementById('mensaje');
-const bold = document.getElementById('bold');
-
-let users = [];
-class user{
-
-    constructor(name, password){
-        this.name=name;
-        this.password=password;
-    }
-}
-
-function login(){
-    messageStatus.textContent='';
-    bold.textContent='';
-    if(inputUser.value != '' || inputPassword.value!=''){
-        for(let i = 0; i<users.length;i++){
-            if(users[i].name==inputUser.value &&users[i].password==inputPassword.value){
-                messageStatus.textContent = 'Login correcto ';
-                bold.textContent=inputUser.value;
-                inputPassword.value = '';
-                return
-            }
+const gasto= {tipoDeGasto,monto};
   
-        }
-        messageStatus.textContent = "login incorrecto";          
+    presupuesto.push(gasto);
 
-    }
-}
+    document.querySelector('ion-input[label="Tipo de Gasto"]').value = '';
+    document.querySelector('ion-input[label="Monto"]').value = '';
 
-function register(){
-  messageStatus.textContent = '';
-  bold.textContent='';
-  if(inputUser.value != '' || inputPassword.value!=''){
-      newUser = new user(inputUser.value,inputPassword.value);
-      users.push(newUser);
-      messageStatus.textContent = 'Registro con exito ';
-      bold.textContent=inputUser.value;
-      inputPassword.value = '';
-  }
-  else{
-      messageStatus.textContent ='Porfavor llene todos los campos';
-  }
-}
-buttonLogin.addEventListener('click', login);
-buttonRegister.addEventListener('click',register);
+    
+    console.log('Información guardada:', gasto);
+  }; 
+
+  
+const botonGuardar = document.querySelector('ion-button');
+botonGuardar.addEventListener('click', guardar);
